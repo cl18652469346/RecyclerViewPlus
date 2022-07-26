cbaf0a47efeab40ee19ef064a25d0ff88e4e983d
cl18652469346 <1285177533@qq..com>
2022-07-26 21:45:43 +0800

    github connect.

---
+++ app/src/main/java/com/cl/recycleviewplus/MultiItemTypeAdapter.java
@@ -0,0 +1,23 @@
+package com.cl.recycleviewplus;
+
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MultiItemTypeAdapter extends RecyclerView.Adapter<ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
